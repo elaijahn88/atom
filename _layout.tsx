5a3454fcc5e97dcddfe96398e6416572bae3130e
Elijah <elajahn8@gmail.com>
2025-10-11 23:20:00 +0300

    Update _layout.tsx

--- _layout.tsx
+++ _layout.tsx
@@ -34,16 +34,16 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Atom"
         options={{
-          title: 'Explore',
+          title: 'Atom',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="details"
+        name="money"
         options={{
-          title: 'video',
+          title: 'money',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="tv.fill" color={color} />,
         }}
       />
