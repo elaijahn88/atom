38960064d2a452c7e92c1374e39c6185dcee139c
Elijah <elajahn8@gmail.com>
2025-10-13 01:30:48 +0300

    Add files via upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,41 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function TabLayout() {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../font.ttf'),
+  });
+
+  if (!loaded) {
+    return null; // Wait for fonts to load
+  }
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="Atom"
-        options={{
-          title: 'Atom',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="money"
-        options={{
-          title: 'money',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="tv.fill" color={color} />,
-        }}
-      />
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        {/* Main tab navigator */}
+        <Stack.Screen name="atom" options={{ headerShown: false }} />
 
-      
-    </Tabs>
+        {/* Not found screen */}
+        <Stack.Screen name="money" />
+
+        {/* Additional screens */}
+        <Stack.Screen 
+          name="Atom" 
+          options={{ title: 'D', headerShown: true }} 
+        />
+        <Stack.Screen 
+          name="index" 
+          options={{ title: 'Profile', headerShown: true }} 
+        />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
