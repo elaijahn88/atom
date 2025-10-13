13438239ddd6f5ca7f3a44fb7298adb29ef2b421
Elijah <elajahn8@gmail.com>
2025-10-13 04:45:31 +0300

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@hooks/haptic-tab';
-import { IconSymbol } from '@hooks/icon-symbol';
-import { Colors } from '@hooks/theme';
-import { useColorScheme } from '@hooks/use-color-scheme';
+import { HapticTab } from '../hooks/haptic-tab';
+import { IconSymbol } from ../hooks/icon-symbol';
+import { Colors } from '../hooks/theme';
+import { useColorScheme } from '../hooks/use-color-scheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
