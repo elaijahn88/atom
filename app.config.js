6ab83cb330aa6ef824a1a20fe35b551997d87f08
Elijah <elajahn8@gmail.com>
2025-10-13 15:45:29 +0300

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -2,6 +2,9 @@ export default {
   expo: {
     name: "jovan",
     slug: "jovan",
+      android: {
+      package: "com.mycompany.myapp",
+    },
     extra: {
       eas: {
         projectId: "a7129ccc-f8a8-4763-8040-ce11e0d7ab40",
