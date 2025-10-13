421e53779e72f8a712ff7d9062ded3b40807f3c7
Elijah <elajahn8@gmail.com>
2025-10-13 15:38:33 +0300

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,11 @@
+export default {
+  expo: {
+    name: "MyApp",
+    slug: "my-app",
+    extra: {
+      eas: {
+        projectId: "a7129ccc-f8a8-4763-8040-ce11e0d7ab40",
+      },
+    },
+  },
+};
