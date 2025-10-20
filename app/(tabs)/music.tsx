d7a7bb67471d309966df16e969a6cab913f22e21
Elijah <elajahn8@gmail.com>
2025-10-20 14:48:29 +0300

    Update music.tsx

--- app/(tabs)/music.tsx
+++ app/(tabs)/music.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StatusBar, TouchableOpacity, ActivityIndicator, StyleSheet, Dimensions } from "react-native";
 import Video from "react-native-video";
 import { Ionicons } from "@expo/vector-icons";
-import { useMediaAutoPlayAds } from "./logic/music";
+import { useMediaAutoPlayAds } from "../logic/music";
 
 const { width, height } = Dimensions.get("window");
 
