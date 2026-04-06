f7aae979f14d289d26d928e3528137a75c1a2c2b
Elijah <elajahn8@gmail.com>
2026-04-06 16:29:22 +0300

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,7 +40,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Food',
+          title: 'Agent',
           tabBarIcon: ({ color }) => (
             <IconSymbol
               size={28}
