07f6a67dd5a50330b42141dcfb624e43b8f85706
Elijah <elajahn8@gmail.com>
2026-04-14 04:07:02 +0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,65 +1,46 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
-import { pickImage, takePhoto, pickFile } from "../lib/file";
+import React from "react";
+import { Engine, Scene } from "react-babylonjs";
+import { Vector3, HemisphericLight, ArcRotateCamera, MeshBuilder } from "@babylonjs/core";
 
-const PickerComponent = () => {
-  const [data, setData] = useState<any>(null);
-
-  const handleGallery = async () => {
-    const res = await pickImage();
-    if (res) setData(res);
-  };
-
-  const handleCamera = async () => {
-    const res = await takePhoto();
-    if (res) setData(res);
-  };
-
-  const handleFile = async () => {
-    const res = await pickFile();
-    if (res) setData(res);
-  };
-
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Picker Component</Text>
+    <Engine canvasId="babylon-canvas" adaptToDeviceRatio antialias>
+      <Scene>
+        
+        {/* Camera */}
+        <arcRotateCamera
+          name="camera"
+          target={new Vector3(0, 0, 0)}
+          alpha={Math.PI / 2}
+          beta={Math.PI / 2}
+          radius={6}
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleGallery}>
-        <Text style={styles.buttonText}>Pick Image</Text>
-      </TouchableOpacity>
+        {/* Light */}
+        <hemisphericLight
+          name="light"
+          intensity={0.8}
+          direction={new Vector3(0, 1, 0)}
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleCamera}>
-        <Text style={styles.buttonText}>Take Photo</Text>
-      </TouchableOpacity>
+        {/* Sphere */}
+        <sphere
+          name="sphere"
+          diameter={2}
+          segments={16}
+          position={new Vector3(0, 1, 0)}
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleFile}>
-        <Text style={styles.buttonText}>Pick File</Text>
-      </TouchableOpacity>
+        {/* Ground */}
+        <ground
+          name="ground"
+          width={10}
+          height={10}
+        />
 
-      {data && (
-        <View style={styles.preview}>
-          <Text>Type: {data.type}</Text>
-          <Text>Name: {data.fileName || data.name}</Text>
-          {data.type === "image" && <Image source={{ uri: data.uri }} style={styles.image} />}
-          {data.type === "file" && <Text>Size: {data.size} bytes</Text>}
-        </View>
-      )}
-    </View>
+      </Scene>
+    </Engine>
   );
 };
 
-export default PickerComponent;
-
-const styles = StyleSheet.create({
-  container: { padding: 20, flex: 1 },
-  title: { fontSize: 20, marginBottom: 10 },
-  button: {
-    backgroundColor: "#333",
-    padding: 12,
-    marginVertical: 5,
-    borderRadius: 8,
-  },
-  buttonText: { color: "#fff", textAlign: "center" },
-  preview: { marginTop: 15 },
-  image: { width: 200, height: 200, marginTop: 10, borderRadius: 10 },
-});
+export default App;
